8f6420f7e3968d2c3deb51361fae37a1ddb9b293
Szymon Rogowski <szymon0r0official@gmail.com>
2022-12-24 01:45:26 +0100

    Setting up

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import json
+
+import uvicorn as uvicorn
 from fastapi import *
 from pymongo import MongoClient
 
-client = MongoClient("")
-database = client[""]
-collection = database[""]
-
 app = FastAPI()
 
-app.
+@app.get("/", tags=["Root"])
+async def read_root():
+    return {"message": "Welcome to this fantastic app!"}
 
-if __name__ == '__main__':
-    app.run(debug=True, port=8080)
+if __name__ == "__main__":
+    uvicorn.run("server.app:app", host="0.0.0.0", port=8080, reload=True)
